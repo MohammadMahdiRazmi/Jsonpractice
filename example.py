334e0acb95c8a53b2750baed10746e3805a59d5b
MahdiRazmi <109846619+MohammadMahdiRazmi@users.noreply.github.com>
2023-10-24 10:19:34 +0330

    Create example.py

---
+++ example.py
@@ -0,0 +1,12 @@
+import json
+place1={"province":"Hormozgan",
+        "city":"bandar abbas",
+        "yy":2023,"mm":10,
+        "dd":24,
+        "hh":11,
+        "mm":17,
+        "ss":51,
+        "Richter":4.1
+}
+loads_place1=json.dumps(place1)
+print(loads_place1)
